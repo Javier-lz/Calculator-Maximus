66712e32b25a3738b06ca62ab5eb56d0edf3924f
Alf-caput <alfre.roble.aba@gmail.com>
2024-12-05 18:06:43 +0100

    Add division

--- calculator.py
+++ calculator.py
@@ -1,3 +1,10 @@
+def division(a, b):
+    try:
+        return a / b
+    except ZeroDivisionError:
+        print("Error: No se puede dividir por cero")
+        return None
+
 def main():
     return
 
